59304088f63f625d5bc38605f2e78c4f64f335ad
luosoy <luosoy@020121386-NB>
2018-03-25 10:04:50 +0800

    提交

--- src/main/webapp/assets/index/js/index.js
+++ src/main/webapp/assets/index/js/index.js
@@ -1,16 +1,22 @@
-$(function(){
+$(function () {
 
     $.send({
-        url:'test/test',
-        type:'post',
-        isjson:true,
-        data:{
-            test:"123123",
-            name:"张三",
-            date:"2018-01-01"
+        url: 'test/test',
+        type: 'post',
+        isjson: true,
+        data: {
+            test: "123123",
+            name: "张三",
+            date: "2018-01-01"
         },
-        success:function(result){
+        success: function (result) {
             console.log(result);
+        },
+        beforeSend: function () {
+            $("body").mask(true);
+        },
+        complete: function () {
+            $("body").mask(false);
         }
     });
 
