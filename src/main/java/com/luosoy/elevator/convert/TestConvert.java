a5b55cf2bc97881ba6863a2e0ad73fa8e813ec57
luosoy <luosoy@020121386-NB>
2018-03-27 17:06:15 +0800

    提交

--- src/main/java/com/luosoy/elevator/convert/TestConvert.java
+++ src/main/java/com/luosoy/elevator/convert/TestConvert.java
@@ -5,12 +5,18 @@ import com.github.pagehelper.PageInfo;
 import com.luosoy.elevator.dto.TestDTO;
 import com.luosoy.elevator.model.Test;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TestConvert {
 
+    @Mappings({
+            @Mapping(target = "firstPage", ignore = true),
+            @Mapping(target = "lastPage", ignore = true)
+    })
     PageInfo<TestDTO> testCovert(PageInfo<Test> testPage);
 
     Test dto2Model(TestDTO testDTO);
