dee665a73ba90d6e9758fa24c918d3b83aa2ea62
luosoy <luosoy@020121386-NB>
2018-03-29 10:27:25 +0800

    提交

---
+++ elevator-web/src/main/java/com/luosoy/elevator/socket/EchoServerHandler.java
@@ -0,0 +1,92 @@
+package com.luosoy.elevator.socket;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+
+@Service
+public class EchoServerHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    private static final Logger logger = LoggerFactory.getLogger(EchoServerHandler.class);
+
+    /*
+     * channelAction
+     *
+     * channel 通道 action 活跃的
+     *
+     * 当客户端主动链接服务端的链接后，这个通道就是活跃的了。也就是客户端与服务端建立了通信通道并且可以传输数据
+     *
+     */
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        logger.info(ctx.channel().localAddress().toString() + " 通道已激活！");
+    }
+
+    /*
+     * channelInactive
+     *
+     * channel 通道 Inactive 不活跃的
+     *
+     * 当客户端主动断开服务端的链接后，这个通道就是不活跃的。也就是说客户端与服务端的关闭了通信通道并且不可以传输数据
+     *
+     */
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        logger.info(ctx.channel().localAddress().toString() + " 通道不活跃！");
+    }
+
+    /**
+     *
+     * @author Taowd
+     * TODO  此处用来处理收到的数据中含有中文的时  出现乱码的问题
+     * 2017年8月31日 下午7:57:28
+     * @param buf
+     * @return
+     */
+    private String getMessage(ByteBuf buf) {
+        byte[] con = new byte[buf.readableBytes()];
+        buf.readBytes(con);
+        try {
+            return new String(con, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        String rev = getMessage(msg);
+        System.out.println("客户端收到服务器数据:" + rev);
+
+        String sendInfo = "Hello 这里是服务端  你好啊！";
+
+        ctx.writeAndFlush(Unpooled.copiedBuffer(sendInfo, CharsetUtil.UTF_8));
+    }
+
+
+    /**
+     * 功能：读取完毕客户端发送过来的数据之后的操作
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        logger.info("服务端接收数据完毕..");
+        // 第一种方法：写一个空的buf，并刷新写出区域。完成后关闭sock channel连接。
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    /**
+     * 功能：服务端发生异常的操作
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+        logger.error("异常信息:" + cause.getMessage(),cause);
+    }
+}
---
+++ elevator-web/src/main/java/com/luosoy/elevator/socket/NettyServer.java
@@ -0,0 +1,83 @@
+package com.luosoy.elevator.socket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.bytes.ByteArrayEncoder;
+import io.netty.handler.codec.string.StringEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.nio.charset.Charset;
+
+@Component
+public class NettyServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(NettyServer.class);
+
+    @Autowired
+    private EchoServerHandler echoServerHandler;
+
+    private Channel channel;
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
+
+    @Value("${rpcServer.ioThreadNum}")
+    int ioThreadNum;
+    //内核为此套接口排队的最大连接个数，对于给定的监听套接口，内核要维护两个队列，未链接队列和已连接队列大小总和最大值
+
+    @Value("${rpcServer.backlog}")
+    int backlog;
+
+    @Value("${rpcServer.port}")
+    int port;
+
+    @PostConstruct
+    public void start() throws InterruptedException {
+        logger.info("begin to start rpc server");
+        bossGroup = new NioEventLoopGroup();
+        workerGroup = new NioEventLoopGroup(ioThreadNum);
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, backlog)
+                .localAddress(port)// 绑定监听端口
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        socketChannel.pipeline()
+                                .addLast(new StringEncoder(Charset.forName("UTF-8")))
+                                .addLast(new ByteArrayEncoder())
+                                .addLast(echoServerHandler); //客户端来接进来的操作
+                    }
+                });
+        channel = serverBootstrap.bind().sync().channel();
+    }
+
+    @PreDestroy
+    public void stop() {
+        logger.info("destroy server resources");
+        if (null == channel) {
+            logger.error("server channel is null");
+        }
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+        channel.closeFuture().syncUninterruptibly();
+        bossGroup = null;
+        workerGroup = null;
+        channel = null;
+    }
+}
